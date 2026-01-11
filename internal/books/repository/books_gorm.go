a3fef7dd37aff9aced63b52dc525983505b82203
dreezy305 <bankoleidris@gmail.com>
2026-01-11 16:45:40 +0100

    books package: handerls, service, repository

--- internal/books/repository/books_gorm.go
+++ internal/books/repository/books_gorm.go
@@ -1,6 +1,12 @@
 package repository
 
-import "gorm.io/gorm"
+import (
+	"errors"
+	"fmt"
+
+	"github.com/dreezy305/library-core-service/internal/model"
+	"gorm.io/gorm"
+)
 
 type GormBookRepository struct {
 	DB *gorm.DB
@@ -10,15 +16,32 @@ func NewGormBookRepository(db *gorm.DB) *GormBookRepository {
 	return &GormBookRepository{DB: db}
 }
 
+func (r *GormBookRepository) BookExist(title string) (bool, error) {
+	var count int64
+	err := r.DB.Model(&model.BookEntity{}).Where("title = ?", title).Count(&count).Error
+	if count > 0 {
+		return true, nil
+	}
+	if err != nil {
+		return false, err
+	}
+	return false, nil
+}
+
+func (r *GormBookRepository) CreateBook(b *model.BookEntity) error {
+	err := r.DB.Create(b).Error
+	if err != nil {
+		fmt.Println(err)
+		return errors.New("failed to create book")
+	}
+	return nil
+}
+
 func (r *GormBookRepository) GetBooks() error {
 	return nil
 }
 
 func (r *GormBookRepository) GetBook(bookId string) error {
-	return nil
-}
-
-func (r *GormBookRepository) CreateBook() error {
 	return nil
 }
 
