58cc321118c734ae2d31770186d3733e383a891e
dreezy305 <bankoleidris@gmail.com>
2026-02-11 00:05:32 +0100

    order entity and order item entity, order gorm

--- internal/orders/repository/order_gorm.go
+++ internal/orders/repository/order_gorm.go
@@ -1,6 +1,9 @@
 package repository
 
-import "gorm.io/gorm"
+import (
+	"github.com/dreezy305/library-core-service/internal/model"
+	"gorm.io/gorm"
+)
 
 type GormOrderRepository struct {
 	DB *gorm.DB
@@ -8,4 +11,26 @@ type GormOrderRepository struct {
 
 func NewGormOrderRepository(db *gorm.DB) *GormOrderRepository {
 	return &GormOrderRepository{DB: db}
+}
+
+func (r *GormOrderRepository) CreateOrder(order *model.OrderEntity) error {
+	return r.DB.Create(order).Error
+}
+
+func (r *GormOrderRepository) GetOrderByID(id string) (*model.OrderEntity, error) {
+	var order model.OrderEntity
+	err := r.DB.Preload("Items").Preload("User").First(&order, "id = ?", id).Error
+	if err != nil {
+		return nil, err
+	}
+	return &order, nil
+}
+
+func (r *GormOrderRepository) ListOrdersByUserID(userID string) ([]*model.OrderEntity, error) {
+	var orders []*model.OrderEntity
+	err := r.DB.Preload("Items").Preload("User").Where("user_id = ?", userID).Find(&orders).Error
+	if err != nil {
+		return nil, err
+	}
+	return orders, nil
 }
